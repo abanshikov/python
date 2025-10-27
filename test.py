2b29c4edb4c1128e6e433df513ecd213bbffddb9
abanshikov <abanshikov@yandex.ru>
2025-10-27 13:09:07 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,11 @@
+class ImageFileAcceptor:
+    def __init__(self, extensions):
+        self.__extensions = extensions
 
-Для последовательной обработки файлов из некоторого списка, например:
+    def __call__(self, filenames):
+        result = []
+        for ext in self.__extensions:
+            if ext in filenames:
+                result.append(filenames)
 
-filenames = ["boat.jpg", "web.png", "text.txt", "python.doc", "ava.8.jpg", "forest.jpeg", "eq_1.png", "eq_2.png", "my.html", "data.shtml"]
-Необходимо объявить класс ImageFileAcceptor, который бы выделял только файлы с указанными расширениями.
-
-Для этого предполагается создавать объекты класса командой:
-
-acceptor = ImageFileAcceptor(extensions)
-где extensions - кортеж с допустимыми расширениями файлов, например: extensions = ('jpg', 'bmp', 'jpeg').
-
-А, затем, использовать объект acceptor в стандартной функции filter языка Python следующим образом:
-
-image_filenames = filter(acceptor, filenames)
-Пример использования класса (эти строчки в программе писать не нужно):
-
-filenames = ["boat.jpg", "web.png", "text.txt", "python.doc", "ava.jpg", "forest.jpeg", "eq_1.png", "eq_2.png"]
-acceptor = ImageFileAcceptor(('jpg', 'bmp', 'jpeg'))
-image_filenames = filter(acceptor, filenames)
-print(list(image_filenames))  # ["boat.jpg", "ava.jpg", "forest.jpeg"]
-P.S. Ваша задача только объявить класс ImageFileAcceptor. На экран ничего выводить не нужно. 
+        return result
