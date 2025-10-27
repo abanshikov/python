589d4d117039979c3faad16f9587501b14f4d55f
abanshikov <abanshikov@yandex.ru>
2025-10-27 11:39:38 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,24 @@
-Объявите класс RandomPassword для генерации случайных паролей. Объекты этого класса должны создаваться командой:
+from random import randint
 
-rnd = RandomPassword(psw_chars, min_length, max_length)
-где psw_chars - строка из разрешенных в пароле символов; min_length, max_length - минимальная и максимальная длина генерируемых паролей.
 
-Непосредственная генерация одного пароля должна выполняться командой:
+class RandomPassword:
+    def __init__(self, psw_chars, min_length, max_length):
+        self.__psw_chars = psw_chars
+        self.__min_length = min_length
+        self.__max_length= max_length
 
-psw = rnd()
-где psw - ссылка на строку длиной в диапазоне [min_length; max_length] из случайно выбранных символов строки psw_chars.
+    def __call__(self, *args, **kwargs):
+        psw = ""
+        for i in range(self.__min_length, randint(self.__min_length+1, self.__max_length)):
+            psw += self.__psw_chars[randint(0, len(self.__psw_chars)-1)]
 
-С помощью генератора списка (list comprehension) создайте список lst_pass из трех сгенерированных паролей объектом rnd класса RandomPassword, созданного с параметрами: 
+        return psw
 
 min_length = 5
 max_length = 20
 psw_chars = "qwertyuiopasdfghjklzxcvbnm0123456789!@#$%&*"
-P.S. Выводить на экран ничего не нужно, только создать список из паролей.
 
-P.P.S. Дополнительное домашнее задание: попробуйте реализовать этот же функционал с использованием замыканий функций.
+rnd = RandomPassword(psw_chars, min_length, max_length)
+psw = rnd()
+
+lst_pass = [rnd() for i in range(3)]
