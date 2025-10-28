86b16fd859df1ec0fe6c06b8241f4fcba71b803f
abanshikov <abanshikov@yandex.ru>
2025-10-28 10:13:51 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,26 @@
-class Model:
-    def __init__(self):
-        self.__db = {}
 
-    def query(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            self.__db[key] = value
+Объявите класс WordString, объекты которого создаются командами:
 
-    def __str__(self):
-        """Model: field_1 = value_1, field_2 = value_2, ..., field_N = value_N"""
-        if self.__db:
-            answer = "Model: "
-            for key, value in self.__db.items():
-                answer += f"{key} = {value}, "
-            return answer[:-2]
-        else:
-            return "Model"
+w1 = WordString()
+w2 = WordString(string)
+где string - передаваемая строка. Например:
+
+words = WordString("Курс по Python ООП")
+Реализовать следующий функционал для объектов этого класса:
+
+len(words) - должно возвращаться число слов в переданной строке (слова разделяются одним или несколькими пробелами);
+words(indx) - должно возвращаться слово по его индексу (indx - порядковый номер слова в строке, начиная с 0).
+
+Также в классе WordString реализовать объект-свойство (property):
+
+string - для передачи и считывания строки.
+
+Пример пользования классом WordString (эти строчки в программе писать не нужно):
+
+words = WordString()
+words.string = "Курс по Python ООП"
+n = len(words)
+first = "" if n == 0 else words(0)
+print(words.string)
+print(f"Число слов: {n}; первое слово: {first}")
+P.S. В программе нужно только объявить класс, выводить в консоль ничего не нужно.
