0c9931d9e74db06a93309c50b0f75d2c92ddd1e0
abanshikov <abanshikov@yandex.ru>
2025-10-28 09:51:45 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,40 @@
-class ImageFileAcceptor:
-    def __init__(self, extensions):
-        self.__extensions = extensions
 
-    def __call__(self, filenames):
-        result = []
-        for ext in self.__extensions:
-            if ext in filenames:
-                result.append(filenames)
+import sys
 
-        return result
+# здесь пишите программу
+
+lst_in = list(map(str.strip, sys.stdin.readlines())) # считывание списка из входного потока (эту строчку  
+
+
+
+class Book:
+    def __init__(self, title: str, author: s, pages):
+
+
+
+
+
+
+
+
+
+Объявите класс с именем Book (книга), объекты которого создаются командой:
+
+book = Book(title, author, pages)
+где title - название книги (строка); author - автор книги (строка); pages - число страниц в книге (целое число).
+
+Также при выводе информации об объекте на экран командой:
+
+print(book)
+должна отображаться строчка в формате:
+
+"Книга: {title}; {author}; {pages}"
+
+Например:
+
+"Книга: Муму; Тургенев; 123"
+
+Прочитайте из входного потока строки с информацией по книге командой:
+
+lst_in = list(map(str.strip, sys.stdin.readlines()))
+(строки идут в порядке: title, author, pages). Создайте объект класса Book и выведите его строковое представление в консоль
