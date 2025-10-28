4830a85031a26f16b3b17f5fa82f6dcc1aeee1a2
abanshikov <abanshikov@yandex.ru>
2025-10-28 10:12:55 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,26 +1,17 @@
-Объявите класс с именем Model, объекты которого создаются командой:
+class Model:
+    def __init__(self):
+        self.__db = {}
 
-model = Model()
-Объявите в этом классе метод query() для формирования записи базы данных. Использоваться этот метод должен следующим образом:
+    def query(self, *args, **kwargs):
+        for key, value in kwargs.items():
+            self.__db[key] = value
 
-model.query(field_1=value_1, field_2=value_2, ..., field_N=value_N)
-
-Например:
-
-model.query(id=1, fio='Sergey', old=33)
-Все эти переданные данные должны сохраняться внутри объекта model класса Model. Затем, при выполнении команды:
-
-print(model)
-В консоль должна выводиться информация об объекте в формате:
-
-"Model: field_1 = value_1, field_2 = value_2, ..., field_N = value_N"
-
-Например:
-
-"Model: id = 1, fio = Sergey, old = 33"
-
-Если метод query() не вызывался, то в консоль выводится строка:
-
-"Model"
-
-P.S. В программе нужно только объявить класс, выводить в консоль ничего не нужно.
+    def __str__(self):
+        """Model: field_1 = value_1, field_2 = value_2, ..., field_N = value_N"""
+        if self.__db:
+            answer = "Model: "
+            for key, value in self.__db.items():
+                answer += f"{key} = {value}, "
+            return answer[:-2]
+        else:
+            return "Model"
