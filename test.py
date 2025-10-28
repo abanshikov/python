d38327a4c1cc50d41189538d10186f9005bd57ac
abanshikov <abanshikov@yandex.ru>
2025-10-28 09:58:39 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,40 +1,19 @@
-
 import sys
 
 # здесь пишите программу
 
-lst_in = list(map(str.strip, sys.stdin.readlines())) # считывание списка из входного потока (эту строчку  
+lst_in = list(map(str.strip, sys.stdin.readlines()))
 
 
 
 class Book:
-    def __init__(self, title: str, author: s, pages):
+    def __init__(self, title: str, author: str, pages: int):
+        self.__title: str = title
+        self.__author: str = author
+        self.__pages: int = pages
+
+    def __str__(self):
+        return f"Книга: {self.__title}; {self.__author}; {self.__pages}"
 
 
-
-
-
-
-
-
-
-Объявите класс с именем Book (книга), объекты которого создаются командой:
-
-book = Book(title, author, pages)
-где title - название книги (строка); author - автор книги (строка); pages - число страниц в книге (целое число).
-
-Также при выводе информации об объекте на экран командой:
-
-print(book)
-должна отображаться строчка в формате:
-
-"Книга: {title}; {author}; {pages}"
-
-Например:
-
-"Книга: Муму; Тургенев; 123"
-
-Прочитайте из входного потока строки с информацией по книге командой:
-
-lst_in = list(map(str.strip, sys.stdin.readlines()))
-(строки идут в порядке: title, author, pages). Создайте объект класса Book и выведите его строковое представление в консоль
+book = Book(lst_in[0], lst_in[1], lst_in[2])
