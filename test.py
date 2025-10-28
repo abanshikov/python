e7761bc7d8d9aa466da92dc8ac0ced1641e2be6c
abanshikov <abanshikov@yandex.ru>
2025-10-28 10:57:57 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,31 @@
-Объявите класс с именем Complex для представления и работы с комплексными числами. Объекты этого класса должны создаваться командой:
+from math import sqrt
 
-cm = Complex(real, img)
-где real - действительная часть комплексного числа (целое или вещественное значение); img - мнимая часть комплексного числа (целое или вещественное значение).
 
-Объявите в этом классе следующие объекты-свойства (property):
+class Complex:
+    def __init__(self, real: (int, float), img: (int, float)):
+        self.__real = real
+        self.__img = img
 
-real - для записи и считывания действительного значения;
-img - для записи и считывания мнимого значения.
+    def __setattr__(self, key, value):
+        if key in ("rear", "img") and type(value) not in (int, float):
+            raise ValueError("Неверный тип данных.")
+        object.__setattr__(self, key, value)
 
-При записи новых значений необходимо проверять тип передаваемых данных. Если тип не соответствует целому или вещественному числу, то генерировать исключение командой:
+    def __abs__(self):
+        return sqrt(self.real**2 + self.img**2)
 
-raise ValueError("Неверный тип данных.")
-Также с объектами класса Complex должна поддерживаться функция:
+    @property
+    def real(self):
+        return self.__real
 
-res = abs(cm)
-возвращающая модуль комплексного числа (вычисляется по формуле: sqrt(real*real + img*img) - корень квадратный от суммы квадратов действительной и мнимой частей комплексного числа).
+    @real.setter
+    def real(self, value):
+        self.__real = value
 
-Создайте объект cmp класса Complex для комплексного числа с real = 7 и img = 8. Затем, через объекты-свойства real и img измените эти значения на real = 3 и img = 4. Вычислите модуль полученного комплексного числа (сохраните результат в переменной c_abs).
+    @property
+    def img(self):
+        return self.__img
 
-P.S. На экран ничего выводить
+    @img.setter
+    def img(self, value):
+        self.__img = value
