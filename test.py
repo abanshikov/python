b9a7080b0ee5c2e16d5b43ddcc253738cc18610d
abanshikov <abanshikov@yandex.ru>
2025-10-30 14:12:26 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,28 +1,29 @@
-Перед вами стоит задача выделения файлов с определенными расширениями из списка файлов, например:
+from pathlib import Path
+
 
 filenames = ["boat.jpg", "ans.web.png", "text.txt", "www.python.doc", "my.ava.jpg", "forest.jpeg", "eq_1.png", "eq_2.xls"]
-Для этого необходимо объявить класс FileAcceptor, объекты которого создаются командой:
 
-acceptor = FileAcceptor(ext1, ..., extN)
-где ext1, ..., extN - строки с допустимыми расширениями файлов, например: 'jpg', 'bmp', 'jpeg'.
 
-После этого предполагается использовать объект acceptor в стандартной функции filter языка Python следующим образом:
+class FileAcceptor:
+    def __init__(self, *args):
+        self.acceptors = set(args)
 
-filenames = list(filter(acceptor, filenames))
-То есть, объект acceptor должен вызываться как функция:
+    def __call__(self, filename):
+        return Path(filename).suffix.replace('.', '') in self.acceptors
 
-acceptor(filename) 
-и возвращать True, если файл с именем filename содержит расширения, указанные при создании acceptor, и False - в противном случае. Кроме того, с объектами класса FileAcceptor должен выполняться оператор:
+    def __add__(self, other):
+        if not isinstance(other, FileAcceptor):
+            raise ArithmeticError("Правый операнд должен быть типом FileAcceptor")
 
-acceptor12 = acceptor1 + acceptor2
-Здесь формируется новый объект acceptor12 с уникальными расширениями первого и второго объектов. Например:
+        tmp = self.acceptors | other.acceptors
+        return FileAcceptor(*tmp)
+
 
 acceptor1 = FileAcceptor("jpg", "jpeg", "png")
 acceptor2 = FileAcceptor("png", "bmp")
 acceptor12 = acceptor1 + acceptor2    # ("jpg", "jpeg", "png", "bmp")
-Пример использования класса (эти строчки в программе писать не нужно):
+# print(acceptor12.acceptors)
 
 acceptor_images = FileAcceptor("jpg", "jpeg", "png")
 acceptor_docs = FileAcceptor("txt", "doc", "xls")
 filenames = list(filter(acceptor_images + acceptor_docs, filenames))
-P.S. На экран в программе ничего выводить не нужно.
