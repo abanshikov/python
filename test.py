4e2b4853b5202a5431a896ee953307c1f20a16a6
abanshikov <abanshikov@yandex.ru>
2025-10-31 11:12:09 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,21 +1,36 @@
-Объявите класс с именем Dimensions, объекты которого создаются командой:
+l_inp = s_inp.split(";")
 
-d = Dimensions(a, b, c)
-где a, b, c - положительные числа (целые или вещественные), описывающие габариты некоторого тела: высота, ширина и глубина.
 
-Каждый объект класса Dimensions должен иметь аналогичные публичные атрибуты a, b, c (с соответствующими числовыми значениями). Также для каждого объекта должен вычисляться хэш на основе всех трех габаритов: a, b, c.
+class Dimensions:
+    def __init__(self, a, b, c):
+        self.a = a
+        self.b = b
+        self.c = c
 
-С помощью функции input() прочитайте из входного потока строку, записанную в формате:
+    def __eq__(self, other):
+        return self.a == other.a and self.b == other.b and self.c == other.c
 
-"a1 b1 c1; a2 b2 c2; ... ;aN bN cN"
+    def __hash__(self):
+        return hash((self.a, self.b, self.c))
 
-Например:
+    def __setattr__(self, key, value):
+        if value  <= 0:
+            raise ValueError("габаритные размеры должны быть положительными числами")
+        object.__setattr__(self, key, value)
 
-"1 2 3; 4 5 6.78; 1 2 3; 0 1 2.5"
+    def __str__(self):
+        return f"a={self.a} b={self.b} c={self.c}"
 
-Если какой-либо габарит оказывается отрицательным значением или равен нулю, то при создании объектов должна генерироваться ошибка командой:
+    def __repr__(self):
+        return f"a={self.a} b={self.b} c={self.c}"
 
-raise ValueError("габаритные размеры должны быть положительными числами")
-Сформируйте на основе прочитанной строки список lst_dims из объектов класса Dimensions. После этого отсортируйте этот список по возрастанию (неубыванию) хэшей этих объектов так, чтобы объекты с равными хэшами стояли друг за другом.
 
-P.S. На экран ничего выводить не нужно
+lst_dims = []
+for string in l_inp:
+    a, b, c = (float(_) for _ in string.split())
+    lst_dims.append(Dimensions(a, b, c))
+
+# print(lst_dims)
+
+lst_dims.sort(key=lambda x: hash(x))
+# print(lst_dims)
