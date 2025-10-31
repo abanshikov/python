0aa1cadbae3c428a23e8efc407a038cb5514b22b
abanshikov <abanshikov@yandex.ru>
2025-10-31 09:29:53 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,12 +1,18 @@
-Объявите в программе класс с именем Rect (прямоугольник), объекты которого создаются командой:
+class Rect:
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
 
-rect = Rect(x, y, width, height)
-где x, y - координата верхнего левого угла (числа: целые или вещественные); width, height - ширина и высота прямоугольника (числа: целые или вещественные).
+    def __eq__(self, other):
+        return self.width == other.width and self.height == other.height
 
-В этом классе определите магический метод, чтобы хэши объектов класса Rect с равными width, height были равны. Например:
+    def __hash__(self):
+        return hash((self.width, self.height))
+
 
 r1 = Rect(10, 5, 100, 50)
 r2 = Rect(-10, 4, 100, 50)
 
 h1, h2 = hash(r1), hash(r2)   # h1 == h2
-P.S. На экран ничего выводить не нужно, только объявить класс.
