4f31d709e934671390eebfa72cb821daf66347f4
abanshikov <abanshikov@yandex.ru>
2025-10-31 10:46:16 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,31 +1,27 @@
-lst_in = [
-    'Python; Балакирев С.М.; 2020',
-    'Python ООП; Балакирев С.М.; 2021',
-    'Python ООП; Балакирев С.М.; 2022',
-    'Python; Балакирев С.М.; 2021',
-]
+class BookStudy:
+    def __init__(self, name, author, year):
+        self.name = name
+        self.author = author
+        self.yeat = year
 
-Из входного потока необходимо прочитать список строк командой:
+    def __eq__(self, other):
+        return self.name.lower() == other.name.lower() and self.author.lower() == other.author.lower()
 
-lst_in = list(map(str.strip, sys.stdin.readlines()))
-Каждая строка содержит информацию об учебном пособии в формате:
+    def __hash__(self):
+        return hash((self.name.lower(), self.author.lower()))
 
-"Название; автор; год издания"
+    def __str__(self):
+        return f"{self.name}: {self.author}"
 
-Например:
+    def __repr__(self):
+        return f"{self.name}: {self.author}"
 
-Python; Балакирев С.М.; 2020
-Python ООП; Балакирев С.М.; 2021
-Python ООП; Балакирев С.М.; 2022
-Python; Балакирев С.М.; 2021
 
-Необходимо каждую из этих строк представить объектом класса BookStudy, которые создаются командой:
+lst_bs = []
+for _ in lst_in:
+    name, author, year = _.split(";")
+    book = BookStudy(name.strip(), author.strip(), int(year))
+    lst_bs.append(book)
 
-bs = BookStudy(name, author, year)
-где name - название пособия (строка); author - автор пособия (строка); year - год издания (целое число). Такие же публичные локальные атрибуты должны быть в объектах класса BookStudy.
-
-Для каждого объекта реализовать вычисление хэша по двум атрибутам: name и author (без учета регистра).
-
-Сформировать список lst_bs из объектов класса BookStudy на основе прочитанных строк (списка lst_in). После этого определить число книг с уникальными хэшами. Это число сохранить через переменную unique_books (целое число).
-
-P.S. На экран ничего выводить не нужно.
+unique_books = len(set(lst_bs))
+#print(unique_books)
