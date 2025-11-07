a91d88fabae833bdf437bd7757644a1c8b901a40
abanshikov <abanshikov@yandex.ru>
2025-11-07 11:33:59 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,43 +1,80 @@
-class Furniture:
-    def __init__(self, name: str, weight: (int, float)):
-        self.__verify_name(name)
-        self.__verify_weight(weight)
-        self._name = name
-        self._weight = weight
+Своей работой вы немного впечатлили начальство и оно поручило вам доделать паттерн слушатель (listener). Идея этого паттерна очень проста и основа реализуется следующим образом:
 
-    @staticmethod
-    def __verify_name(name):
-        if not type(name) == str:
-            raise TypeError('название должно быть строкой')
+class Observer:
+    def update(self, data):
+        pass
 
-    @staticmethod
-    def __verify_weight(weight):
-        if weight <= 0:
-            raise TypeError('вес должен быть положительным числом')
+    def __hash__(self):
+        return hash(id(self))
 
 
-class Closet(Furniture):
-    def __init__(self, name, weight, tp, doors):
-        super().__init__(name, weight)
-        self._tp = tp
-        self._doors = doors
+class Subject:
+    def __init__(self):
+        self.__observers = {}
+        self.__data = None
 
-    def get_attrs(self):
-        return self._name, self._weight, self._tp, self._doors
+    def add_observer(self, observer):
+        self.__observers[observer] = observer
 
-class Chair(Furniture):
-    def __init__(self, name, weight, height):
-        super().__init__(name, weight)
-        self._height = height
+    def remove_observer(self, observer):
+        if observer in self.__observers:
+            self.__observers.pop(observer)
 
-    def get_attrs(self):
-        return self._name, self._weight, self._height
+    def __notify_observer(self):
+        for ob in self.__observers:
+            ob.update(self.__data)
 
-class Table(Furniture):
-    def __init__(self, name, weight, height, square):
-        super().__init__(name, weight)
-        self._height = height
-        self._square = square
+    def change_data(self, data):
+        self.__data = data
+        self.__notify_observer()
+Здесь в объектах класса Subject можно зарегистрировать (добавить) множество объектов класса Observer (наблюдатель, слушатель). Это делается с помощью метода add_observer(). Затем, когда данные (self.__data) меняются путем вызова метода change_data() класса Subject, то у всех слушателей автоматически вызывается метод update(). В этом методе можно прописать самую разную логику работы при изменении данных в каждом конкретном слушателе.
 
-    def get_attrs(self):
-        return self._name, self._weight, self._height, self._square
+В проекте данный паттерн предполагается использовать для отображения информации о погоде в различных форматах:
+
+- текущая температура;
+- текущее атмосферное давление;
+- текущая влажность воздуха.
+
+Для этого сами данные определяются классом:
+
+class Data:
+    def __init__(self, temp, press, wet):
+        self.temp = temp    # температура
+        self.press = press  # давление
+        self.wet = wet      # влажность
+А вам поручается разработать дочерние классы, унаследованные от класса Observer, с именами:
+
+TemperatureView - слушатель для отображения информации о температуре;
+PressureView - слушатель для отображения информации о давлении;
+WetView - слушатель для отображения информации о влажности.
+
+Каждый из этих классов должен переопределять метод update() базового класса так, чтобы выводилась в консоль информация в формате:
+
+TemperatureView: "Текущая температура <число>"
+PressureView: "Текущее давление <число>"
+WetView: "Текущая влажность <число>"
+
+Важно: для вывода информации в консоль используйте функцию print() с одним аргументом в виде F-строки.
+
+Пример использования классов (эти строчки в программе писать не нужно):
+
+subject = Subject()
+tv = TemperatureView()
+pr = PressureView()
+wet = WetView()
+
+subject.add_observer(tv)
+subject.add_observer(pr)
+subject.add_observer(wet)
+
+subject.change_data(Data(23, 150, 83))
+# выведет строчки:
+# Текущая температура 23
+# Текущее давление 150
+# Текущая влажность 83
+subject.remove_observer(wet)
+subject.change_data(Data(24, 148, 80))
+# выведет строчки:
+# Текущая температура 24
+# Текущее давление 148
+P.S. В программе нужно объявить только классы. На экран выводить ничего не нужно.
