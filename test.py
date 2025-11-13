2f0579d8e8e8f6f4652ffc799b5db53af13c6c2e
abanshikov <abanshikov@yandex.ru>
2025-11-13 13:51:43 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,18 @@
-С помощью декоратора dataclass и функции field:
-
 from dataclasses import dataclass, field
-объявите Data Class с именем Velocity и следующим набором полей (порядок важен):
 
-model (str: с исключением из операций сравнения);
-speed (float: с исключением из параметров инициализатора и начальным значением 0);
-weight (float: вес);
-dims (tuple: с исключением из операций сравнения и метода __repr__ и начальным значением None).
-Создайте два объекта с именами vl1, vl2 класса Velocity с данными:
 
-vl1: model="car"; weight=5.4; dims=(100, 20, 30);
-vl2: model="ship"; weight=5.4; dims=(500, 200, 130).
-Выполните сравнение на равенство этих двух объектов. Результат (булево значение) сохраните в переменной res.
+@dataclass
+class Velocity:
+    model: str = field(compare=False)
+    speed: float = field(init=False, default=0)
+    weight: float
+    dims: tuple = field(default_factory=tuple, repr=False)
 
-P.S. На экран ничего выводить не нужно.
+    def __post_init__(self):
+        self.dims = None
+
+
+vl1 = Velocity(model="car", weight=5.4, dims=(100, 20, 30))
+vl2 = Velocity(model="ship", weight=5.4, dims=(500, 200, 130))
+
+res = vl1 == vl2
