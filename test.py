d8e38a4eb5dc41c1e5de35d5e52b8afb4de7c1c6
abanshikov <abanshikov@yandex.ru>
2025-11-13 13:43:57 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,19 +1,20 @@
-С помощью декоратора dataclass и функции field:
-
 from dataclasses import dataclass, field
-объявите Data Class с именем PolyLine и следующим набором полей (порядок важен):
 
-width (float: с исключением из операций сравнения);
-color (int: с исключением из операций сравнения и начальным значением 0);
-points (list: список с начальным значением кортежа (0, 0), то есть, первый элемент списка - это кортеж (0, 0)).
-Создайте два объекта с именами pl1, pl2 класса PolyLine с данными:
 
-pl1: width=0.5; color=0;
-pl2: width=1.5; color=2.
-После этого добавьте в объекты следующие координаты (в список points):
+@dataclass
+class PolyLine:
+    width: float = field(compare=False)
+    color: int = field(compare=False, default=0)
+    points: list = field(default_factory=list)
 
-pl1: [(10, -5), (12, 1)];
-pl2: [(10, -5), (12, 1)].
-Выполните сравнение на равенство этих двух объектов. Результат (булево значение) сохраните в переменной res.
+    def __post_init__(self):
+        self.points = [(0, 0)]
 
-P.S. На экран ничего выводить не нужно
+
+pl1 = PolyLine(width=0.5, color=0)
+pl2 = PolyLine(width=1.5, color=2)
+
+pl1.points.extend([(10, -5), (12, 1)])
+pl2.points.extend([(10, -5), (12, 1)])
+
+res = pl1 == pl2
