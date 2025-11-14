0f6270920d94562f8496d1d89e9035e19527bb76
abanshikov <abanshikov@yandex.ru>
2025-11-14 09:46:54 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,44 +1,21 @@
-В программе ниже объявлен класс Thing следующим образом:
+Продолжите программу, в которой с помощью декоратора dataclass объявите класс Animal со следующим набором полей (порядок важен):
 
-from dataclasses import dataclass
-from typing import Any
+name (str: имя животного);
 
+old (int: возраст животного);
 
-@dataclass
-class Thing:
-    name: Any
-    color: Any
-    weight: float
-Необходимо продолжить эту программу и объявить дочерний класс Table от базового класса Thing, как Data Class, используя декоратор dataclass. Дочерний класс Table должен иметь следующие поля (порядок важен):
+weight (Any: вес животного).
 
-width (float: ширина стола);
-height (float: высота стола).
-Создайте объект tb класса Table со следующим набором данных:
+На основе класса Animal с помощью декоратора dataclass объявите дочерний класс Turtle (черепаха) со следующим набором полей (порядок важен):
 
-name: "Suprise"; color: "red"; weight: 102.5; width: 0.45; height: 10.1
+weight (float: вес черепахи);
+
+length (float: длина черепахи);
+
+speed (float: скорость черепахи, с исключением из параметров инициализатора и начальным значением 0).
+
+Создайте объект t класса Turtle со следующим набором данных:
+
+name: "Черя"; old: 94; weight: 3.5; length: 108
 
 P.S. На экран ничего выводить не нужно
-
-
-
-from dataclasses import dataclass
-from typing import Any
-
-
-@dataclass
-class Thing:
-    name: Any
-    color: Any
-    weight: float
-
-
-@dataclass
-class Table(Thing):
-    width: float
-    height: float
-
-
-tb = Table(name="Suprise", color="red", weight=102.5, width=0.45, height=10.1)
-
-
-print(tb)
