103e52429e42aaf9ef403f90d76dbfd976e27354
abanshikov <abanshikov@yandex.ru>
2025-11-14 09:45:41 +0700

    Update test.py

--- test.py
+++ test.py
@@ -18,3 +18,27 @@ height (float: высота стола).
 name: "Suprise"; color: "red"; weight: 102.5; width: 0.45; height: 10.1
 
 P.S. На экран ничего выводить не нужно
+
+
+
+from dataclasses import dataclass
+from typing import Any
+
+
+@dataclass
+class Thing:
+    name: Any
+    color: Any
+    weight: float
+
+
+@dataclass
+class Table(Thing):
+    width: float
+    height: float
+
+
+tb = Table(name="Suprise", color="red", weight=102.5, width=0.45, height=10.1)
+
+
+print(tb)
