4a7b28cf5285ccfd31744e9d049a94c6eb8d6cc8
abanshikov <abanshikov@yandex.ru>
2025-12-17 10:16:50 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,26 +1,11 @@
-class PrimaryKeyError(Exception):
-    def __init__(self, id = None, pk = None):
-        if id:
-            self.message = f"Значение первичного ключа id = {id} недопустимо"
-        elif pk:
-            self.message = f"Значение первичного ключа pk = {pk} недопустимо"
-        else:
-            self.message = "Первичный ключ должен быть целым неотрицательным числом"
+Объявите класс модели с именем ShopItem для работы с таблицей следующей структуры:
 
-    def __str__(self):
-        return self.message
+id: primary key (в модели не прописывается)
+name: CharField - строка с максимальной длиной 100 символов; обязательное поле;
+weight: IntegerField - вес товара с начальным значением 0; не обязательное поле;
+price: IntegerField - цена товара с начальным значением 0; обязательное поле;
+is_exists: BooleanField - наличие товара (True - присутствует; False - отсутствует); по умолчанию True.
 
+Используя метод all() стандартного менеджера записей класса ShopItem, выберите все записи из соответствующей таблицы БД и сохраните их в переменной records.
 
-e1 = PrimaryKeyError()          # Первичный ключ должен быть целым неотрицательным числом
-e2 = PrimaryKeyError(id='abc')  # Значение первичного ключа id = abc недопустимо
-e3 = PrimaryKeyError(pk='123')  # Значение первичного ключа pk = 123 недопустимо
-
-
-print(e2) # Значение первичного ключа id = abc недопустимо
-
-
-try:
-    e2 = PrimaryKeyError(id=-10.5)
-    raise e2
-except PrimaryKeyError:
-    print(e2)
+P.S. На экран ничего выводить не нужно
