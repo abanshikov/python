8bcd7818e3f5700ee7f3a4bd1d7139f91b68746d
abanshikov <abanshikov@yandex.ru>
2025-12-17 10:59:17 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,17 +1,11 @@
+Пусть имеется следующий класс модели:
+
 from django.db import models
 
+class Person(models.Model):
+    full_name = models.CharField(max_length=255)
+    salary = models.PositiveIntegerField(default=0)
+    job = models.CharField(max_length=255)
+Используя метод filter() стандартного менеджера записей класса Person, выберите все записи с параметром job равным "Программист".
 
-class Auto(models.Model):
-    name = models.CharField(max_length=100)  # строка с максимальной длиной 100 символов; обязательное поле;
-    model = models.CharField()  # марка машины; обязательное поле;
-    price = models.IntegerField(default=0, blank=True)  # цена машины с начальным значением 0; необязательное поле;
-    distance = models.IntegerField(default=0, blank=True)  # пробег машины с начальным значением 0; необязательное поле;
-    is_exists = models.BooleanField(default=True)  # наличие товара (True - присутствует; False - отсутствует); по умолчанию True.
-
-
-Auto.objects.create(name="BMW X6", model="BMW", price=6000111)
-Auto.objects.create(name="Toyota Corolla", model="Toyota", distance=72000)
-Auto.objects.create(name="Haval 7", model="Haval", price=3500222)
-
-
-autos = Auto.objects.all()[:2]
+P.S. На экран ничего выводить не нужно
