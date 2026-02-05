512aabd9ae8603cd36474f3b5808047103df3fdf
abanshikov <abanshikov@yandex.ru>
2026-02-05 13:08:11 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,39 +1,12 @@
-Пусть имеется следующий класс модели:
+Объявите класс формы с именем ContactForm, не связанной с моделью, со следующими полями:
 
-from django.db import models
+fio: текстовое поле; максимальная длина 200 символов, обязательное;
+email: поле ввода адреса электронной почты; обязательное;
+city: текстовое поле; максимальная длина 100 символов, необязательное;
+phone: текстовое поле; максимальная длина 12 символов, необязательное;
+agree: поле BooleanField; необязательное;
+content: поле ввода полноценного многострочного текста; обязательное.
 
-class Post(models.Model):
-    slug = models.SlugField(max_length=255, unique=True, db_index=True)
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=False)
-И функция представления для отображения текущей записи по слагу:
+Атрибуты класса должны иметь те же названия и порядок, что и в описании. То есть: fio, email, city и т.д.
 
-def post_show_by_slug(request, ps_slug):
-    post = # здесь прописывайте функцию get_object_or_404()
-    return render(request, 'post/post.html', {'post': post})
-Допишите функцию post_show_by_slug для выбора текущей записи поста модели Post по слагу ps_slug с помощью функции get_object_or_404(). Импортировать функции get_object_or_404() и render() не нужно!
-
-P.S. На экран ничего выводить не нужно
-
-
-
-
-
-
-from django.db import models
-
-class Post(models.Model):
-    slug = models.SlugField(max_length=255, unique=True, db_index=True)
-    title = models.CharField(max_length=255)
-    content = models.TextField(blank=True)
-    time_create = models.DateTimeField(auto_now_add=True)
-    time_update = models.DateTimeField(auto_now=True)
-    is_published = models.BooleanField(default=False)
-
-
-def post_show_by_slug(request, ps_slug):
-    post = # здесь прописывайте функцию get_object_or_404()
-    return render(request, 'post/post.html', {'post': post})
+P.S. На экран ничего выводить не нужно.
