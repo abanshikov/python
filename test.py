efc700fdc094ec26e3621f147ece963b7f3018a1
abanshikov <abanshikov@yandex.ru>
2026-02-05 13:24:32 +0700

    Update test.py

--- test.py
+++ test.py
@@ -1,7 +1,39 @@
-class ContactForm(forms.Form):
-    fio = forms.CharField(max_length=200)
-    email = forms.EmailField()
-    city = forms.CharField(max_length=200, required=False)
-    phone = forms.CharField(max_length=12, required=False)
-    agree = froms.BooleanField(required=False)
-    content = forms.CharField(widget=forms.Textarea)
+Пусть имеется следующий класс формы:
+
+# --------------- forms.py -------------------------
+from django import forms
+
+class AddPostForm(forms.Form):
+    title = forms.CharField(max_length=255)
+    slug = forms.SlugField(max_length=255)
+    content = forms.CharField(widget=forms.Textarea(), required=False)
+    is_published = forms.BooleanField(required=False)
+
+# --------------- views.py -------------------------
+# from django.shortcuts import render
+# from .forms import AddPostForm
+
+# здесь продолжайте программу
+Объявите функцию представления с именем post_new, в которой:
+
+создается пустая форма AddPostForm;
+с помощью функции render формируется HTML-документ по шаблону 'women/addpage.html' с передачей в него объекта формы AddPostForm через переменную (ключ) form.
+P.S. На экран ничего выводить не нужно
+
+
+
+
+# --------------- forms.py -------------------------
+from django import forms
+
+class AddPostForm(forms.Form):
+    title = forms.CharField(max_length=255)
+    slug = forms.SlugField(max_length=255)
+    content = forms.CharField(widget=forms.Textarea(), required=False)
+    is_published = forms.BooleanField(required=False)
+
+# --------------- views.py -------------------------
+# from django.shortcuts import render
+# from .forms import AddPostForm
+
+# здесь продолжайте программу
